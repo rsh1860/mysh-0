df639b3fecbfec365f8cccb5afc39b107d29f32b
rsh1860 <rsh1860@ajou.ac.kr>
2017-10-03 12:56:14 +0900

    tokenize the command from the user(number of arguments and argumanet list)

--- src/utils.c
+++ src/utils.c
@@ -3,5 +3,23 @@
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+
+//the number of arguments
+for(int i=0;;i++)
+	if(command[i]==NULL) break;
+argc=i;
+printf("argc == %d\n", argc);
+
+//the argument list
+char str[100] = char* command;
+**argv = strtok("str", " ");
+
+printf("argv == {);
+while(result != NULL)
+{
+printf(" \"%s\"", result);
+}
+printf(" }");
+
+  
 }
