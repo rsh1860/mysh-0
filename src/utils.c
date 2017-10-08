7dfb5a48291a056a62c56cf403f98bb370edc7e4
rsh1860 <rsh1860@ajou.ac.kr>
2017-10-08 17:02:13 +0900

    no printf

--- src/utils.c
+++ src/utils.c
@@ -1,25 +1,18 @@
 #include "utils.h"
+#include <stdio.h>
+#include <string.h>
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-int i;
-//the number of arguments
-for(i=0;;i++)
-	if(command[i]==null) break;
-argc=i;
-printf("argc == %d\n", argc);
 
-//the argument list
-char str[100] = char* command;
-**argv = strtok("str", " ");
+//memory allocation
+char*buf = NULL;
+buf =( char*) malloc(1000);
 
-printf("argv == {");
-while(result != null)
-{
-printf(" \"%s\"", result);
-}
-printf(" }");
+char *p;
 
-  
+
+
+
 }
