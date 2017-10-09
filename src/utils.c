e177278971f8c52f1e7201c40a42cd99416df29b
rsh1860 <rsh1860@ajou.ac.kr>
2017-10-09 09:30:29 +0900

    complete memory allocation and arguments

--- src/utils.c
+++ src/utils.c
@@ -1,18 +1,37 @@
 #include "utils.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
 
+int n=0;
+
 //memory allocation
-char*buf = NULL;
-buf =( char*) malloc(1000);
-
-char *p;
-
+char*ptr = malloc(sizeof(char)*100);
+*argv = char**malloc(sizeof(char*)*100);
+*argv[n] = char*malloc(sizeof(char)*100);
 
 
+//arguments
+char*cmd;
+cmd = strtok(ptr, "' ', \n, \t");
+if(cmd==NULL)
+{
+*argc=1; strcpy(*argv[0],"");
+return;
+}
+while(cmd!=NULL)
+{
+strcpy((*argv)[n], cmd); ++n;
+*argv[n] = char*malloc(sizeof(char)*100);
+cmd = strtok(NULL, "' ', \n, \t");
+
+*argc = n;
+
+//free
+char free(ptr);
 
 }
