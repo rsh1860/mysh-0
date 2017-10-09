d550265e655546d95cda7a987a337de4c2dfea2c
rsh1860 <rsh1860@ajou.ac.kr>
2017-10-09 09:57:55 +0900

    validate_argv

--- src/commands.c
+++ src/commands.c
@@ -1,12 +1,12 @@
 #include <string.h>
-
+#include <stdio.h>
 #include "commands.h"
 
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
 
-  // TODO: Fill it!
+  
   return 0;
 }
 
@@ -14,17 +14,19 @@ int do_pwd(int argc, char** argv) {
   if (!validate_pwd_argv(argc, argv))
     return -1;
 
-  // TODO: Fill it!
+  
 
   return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
+  if(strcmp(argv[0],"cd")&&argc==2)
   return 1;
+  else return 0;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
+  if(strcmp(argv[0],"pwd")&&argc==1)
   return 1;
+  else return 0;
 }
