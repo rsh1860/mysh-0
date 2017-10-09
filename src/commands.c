2d5dce62965706b2960f647dcb34b27e926a8b2a
rsh1860 <rsh1860@ajou.ac.kr>
2017-10-09 12:02:00 +0900

    complete do_

--- src/commands.c
+++ src/commands.c
@@ -5,18 +5,25 @@
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
-
-  
-  return 0;
+  else
+  {
+    int rst = cndir(argv[1]);
+    if (rst==-1) printf("Error!");
+    else printf("Success!");
+    return 0; 
+  }
 }
 
 int do_pwd(int argc, char** argv) {
   if (!validate_pwd_argv(argc, argv))
     return -1;
-
-  
-
-  return 0;
+  else
+  {
+    char*cmd;
+    cmd = getcwd(NULL, 100);
+    printf("%s\n", cmd);
+    return 0;
+  } 
 }
 
 int validate_cd_argv(int argc, char** argv) {
